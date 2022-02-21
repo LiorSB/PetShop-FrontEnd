582d246b22d1c0f2d7b7981b7609c5eecad5ea8c
Lior <92099051+LiorSB@users.noreply.github.com>
2022-02-21 09:53:47 +0200

    Add files via upload

---
+++ StoreItems.js
@@ -0,0 +1,112 @@
+storeItems= [
+  {
+    "name": "Stretch Leesh",
+    "description": "A strecing leesh that can be stretched up until 3 meters, has a stop and release button.",
+    "price": 90,
+    "picture": "https://d3m9l0v76dty0.cloudfront.net/system/photos/6258379/show/01d8eda89b0168240ea3679553601882.jpg",
+    "quantity": 5,
+    "category": "Pet_Equipment",
+    "EnterStockTime": "2021-02-03",
+    "manufacturer": "FLEXI",
+    "tagArry": [ "Japan", "1 year warranty", "7 days deilvery" ]
+  },
+  {
+    "name": "Collar",
+    "description": "Simple and classy collar. The collar has a moving buckle which can be set to the perfect fit for the pet.",
+    "price": 55,
+    "picture": "https://d3m9l0v76dty0.cloudfront.net/system/photos/6118353/show/f66a18d7f3b36423a86fa541aecd0548.jpg",
+    "quantity": 7,
+    "category": "Pet_Equipment",
+    "EnterStockTime": "2019-02-28",
+    "manufacturer": "MAX & MOLLY",
+    "tagArry": [ "Israel", "no warranty", "4 days deilvery" ]
+  },
+  {
+    "name": "2 Bowls",
+    "description": "Two bowls made from stainless steel hypoallergenic which can contain both food and water.",
+    "price": 46,
+    "picture": "https://d3m9l0v76dty0.cloudfront.net/system/photos/1323979/show/867edab06aa31139b742bf67f450a93a.jpg",
+    "quantity": 12,
+    "category": "Pet_Equipment",
+    "EnterStockTime": "2020-02-01",
+    "manufacturer": "",
+    "tagArry": [ "2 year warranty", "7 days deilvery" ]
+  },
+  {
+    "name": "Easy Bed",
+    "description": "60% cotton, 40% polyester. Padding: 100% polyester. Size: 70*80. Color blue.",
+    "price": 250,
+    "picture": "https://d3m9l0v76dty0.cloudfront.net/system/photos/5953484/show/637060d9876aa91094cb944ab428a026.jpg",
+    "quantity": 2,
+    "category": "Pet_Equipment",
+    "EnterStockTime": "2021-01-03",
+    "manufacturer": "FABOTEX",
+    "tagArry": [ "China", "7 days deilvery" ]
+  },
+  {
+    "name": "Dry Food",
+    "description": "3 kinds of meat which are muscle, internal organs, carilage and bones to provide nutritions in their netural form.",
+    "price": 369,
+    "picture": "https://d3m9l0v76dty0.cloudfront.net/system/photos/6298734/show/2f9bfb08053dbd7211edb886af024a89.jpg",
+    "quantity": 4,
+    "category": "Pet_Food",
+    "EnterStockTime": "2020-09-09",
+    "manufacturer": "ACANA",
+    "tagArry": [ "Japan", "Replacable if pet's don't like", "7 days deilvery" ]
+  },
+  {
+    "name": "Wet Food",
+    "description": "Ingredients: turkey muscle, turkey heart, liver, stomach, neck and breast meat. Contains protein, fat and fiber. 400 gram container",
+    "price": 14,
+    "picture": "https://d3m9l0v76dty0.cloudfront.net/system/photos/6183769/show/01453b27087573cf617fd53e97213737.jpg",
+    "quantity": 22,
+    "category": "Pet_Food",
+    "EnterStockTime": "2020-12-07",
+    "manufacturer": "HAPPY DOG",
+    "tagArry": [ "USA", "Replacable if pet's don't like", "14 days deilvery" ]
+  },
+  {
+    "name": "Fish Oil",
+    "description": "Liquid food supplement, which helps animals that suffer from skin and furr problems. Contains omega 3 calore EPA, omega 6 vitamin E, antioxidants, DHA.",
+    "price": 119,
+    "picture": "https://d3m9l0v76dty0.cloudfront.net/system/photos/5927954/show/71c4b13ea038fbe77bc10f0687e1fef7.jpg",
+    "quantity": 17,
+    "category": "Pet_Food",
+    "EnterStockTime": "2021-02-01",
+    "manufacturer": "DERMA COAT",
+    "tagArry": [ "Italy", "7 days deilvery" ]
+  },
+  {
+    "name": "Fleas and Ticks Protection Collar",
+    "description": "Collar that prevents fleas and ticks, based on netural oils. Doesn't harm the animal or nature.",
+    "price": 58,
+    "picture": "https://d3m9l0v76dty0.cloudfront.net/system/photos/2840626/show/01c4a2743eaef92a6442e4051e486f49.jpg",
+    "quantity": 13,
+    "category": "Animal_Care",
+    "EnterStockTime": "2021-02-03",
+    "manufacturer": "CAMON",
+    "tagArry": [ "1 month testing period" ]
+  },
+  {
+    "name": "Dry Shampoo",
+    "description": "220 ml. Dry shampoo for deep cleaning with the scent of coconut and berries. ",
+    "price": 60,
+    "picture": "https://d3m9l0v76dty0.cloudfront.net/system/photos/5926395/show/148004e645184df45bf32527dad367bc.jpg",
+    "quantity": 2,
+    "category": "Animal_Care",
+    "EnterStockTime": "2019-02-11",
+    "manufacturer": "TROPICLEAN",
+    "tagArry": [ "Free samples", "Doggy treat with every purchase" ]
+  },
+  {
+    "name": "Nail Clipper",
+    "description": "Very safe clipper which prevents injuries to the animal.",
+    "price": 65,
+    "picture": "https://d3m9l0v76dty0.cloudfront.net/system/photos/1323693/show/7f21593b73b874fe850050559b44ac23.jpg",
+    "quantity": 1,
+    "category": "Animal_Care",
+    "EnterStockTime": "2020-05-03",
+    "manufacturer": "FERPLAST",
+    "tagArry": [ "Thailand", "Free lesson how to use", "14 days deilvery" ]
+  }
+]
